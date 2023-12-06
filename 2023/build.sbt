f41a90027a1a1e7363723f7c95e8ffd2f1a69e5f
Elliot Maisl <elliot.maisl@gmail.com>
2023-12-06 23:09:50 +0100

    2023: init

---
+++ 2023/build.sbt
@@ -0,0 +1,10 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.3.1"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "aoc"
+  )
+
+libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.2"
---
+++ 2023/src/main/scala/main.scala
@@ -0,0 +1,12 @@
+import scala.io.Source
+
+@main def main: Unit =
+    val day = 1
+    val part = 1
+
+    val bufferedSource = Source.fromResource(s"input$day.txt")
+    val lines = bufferedSource.getLines().toList
+    bufferedSource.close()
+
+    day match {
+    }
