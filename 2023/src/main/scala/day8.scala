5e8c3f9089108185f54b0ac296640402c8e572f6
Elliot Maisl <elliot.maisl@gmail.com>
2023-12-08 23:44:07 +0100

    2023: day 8

---
+++ 2023/src/main/scala/day8.scala
@@ -0,0 +1,43 @@
+import scala.annotation.tailrec
+
+private val pattern = "(\\w+) = \\((\\w+), (\\w+)\\)".r
+
+extension [K, V](map: Map[K, V])
+    def fetch(key: K): (K, V) = key -> map(key)
+
+def day8(lines: List[String], part: Int = 1): Int =
+    val sequence = lines.head.map {
+        case 'L' => 0
+        case 'R' => 1
+        case char => throw new IllegalArgumentException(s"Unknown sequence char $char")
+    }.toList
+    // Skip the first and second lines
+    val nodes = lines.tail.tail.map {
+        case pattern(name, left, right) => name -> (left, right)
+    }.toMap
+
+    if part == 1 then
+        var node = nodes.fetch("AAA")
+        var total = 0
+        while node._1 != "ZZZ" do
+            node = sequence(total % sequence.size) match {
+                case 0 => nodes.fetch(node._2._1)
+                case 1 => nodes.fetch(node._2._2)
+            }
+            total += 1
+
+        total
+    else
+        var currentNodes = nodes.keySet.filter(_.endsWith("A")).map(nodes.fetch)
+        var total = 0
+        while !currentNodes.forall(_._1.endsWith("Z")) do
+            currentNodes = currentNodes.map { node =>
+                sequence(total % sequence.size) match {
+                    case 0 => nodes.fetch(node._2._1)
+                    case 1 => nodes.fetch(node._2._2)
+                }
+            }
+
+            total += 1
+
+        total
--- 2023/src/main/scala/main.scala
+++ 2023/src/main/scala/main.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 
 @main def main: Unit =
-    val day = 7
+    val day = 8
     val part = 1
 
     val bufferedSource = Source.fromResource(s"input$day.txt")
@@ -15,4 +15,5 @@ import scala.io.Source
         case 4 => println(day4(lines, part))
         case 6 => println(day6(lines, part))
         case 7 => println(day7(lines, part))
+        case 8 => println(day8(lines, part))
     }
